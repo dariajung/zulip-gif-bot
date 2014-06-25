5e0aae57673fa66c30621697c2a7e6eddaf60f9b
Daria Jung <djj2115cu@gmail.com>
2014-06-25 19:12:00 -0400

    parse out client response and normalize query to pass to API call to giphy

--- gifbot.py
+++ gifbot.py
@@ -4,6 +4,7 @@ import zulip
 import sys
 import config
 import json
+import requests
 
 client = zulip.Client(email=config.USERNAME,
                       api_key=config.API_KEY)
@@ -19,7 +20,6 @@ client.send_message({
 def respond(msg):
     if msg['sender_email'] != "gif-bot@students.hackerschool.com":
         content = msg['content'].upper().split()
-        print content
             
         if (content[0] == "GIF" and content[1] == "ME"):
             print "yay gif me"
@@ -31,12 +31,18 @@ def respond(msg):
                 "content": "I don't know what you're talking about :tired_face:"
             })
 
+def call_giphy(search_param):
+    response = requests.get("http://api.giphy.com/v1/gifs/search?limit=20&q=funny+cat&api_key=dc6zaTOxFJmzC")
+
+def normalize_query(arr):
+    query = '+'.join(arr[2:])
+    return query.lower()
+
 # Print each message the user receives
 # This is a blocking call that will run forever
-# client.call_on_each_message(lambda msg: sys.stdout.write(str(msg) + "\n"))
 client.call_on_each_message(lambda msg: respond(msg))
 
 # Print every event relevant to the user
 # This is a blocking call that will run forever
 # This will never be reached unless you comment out the previous line
-client.call_on_each_event(lambda msg: sys.stdout.write(str(msg) + "\n"))
+#client.call_on_each_event(lambda msg: sys.stdout.write(str(msg) + "\n"))
