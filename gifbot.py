edf33e557f4782019562dc0b7bfb3677aa6cdf07
Daria Jung <djj2115cu@gmail.com>
2014-06-25 20:26:10 -0400

    yay it works! get gif link from giphy api

--- gifbot.py
+++ gifbot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import zulip
-import sys
 import config
 import json
 import requests
+import random
 
 client = zulip.Client(email=config.USERNAME,
                       api_key=config.API_KEY)
@@ -22,7 +22,15 @@ def respond(msg):
         content = msg['content'].upper().split()
             
         if (content[0] == "GIF" and content[1] == "ME"):
-            print "yay gif me"
+            normalized = normalize_query(content)
+            api_call = "http://api.giphy.com/v1/gifs/search?limit=20&q=%s&api_key=dc6zaTOxFJmzC" % normalized
+            img_url = call_giphy(api_call)
+
+            client.send_message({
+                "type": "private",
+                "to": config.EMAIL,
+                "content": "%s" % img_url
+            })
 
         else:
             client.send_message({
@@ -31,9 +39,14 @@ def respond(msg):
                 "content": "I don't know what you're talking about :tired_face:"
             })
 
-def call_giphy(search_param):
-    response = requests.get("http://api.giphy.com/v1/gifs/search?limit=20&q=funny+cat&api_key=dc6zaTOxFJmzC")
+def call_giphy(api_url):    
+    response = requests.get(api_url).content
+    loaded_json = json.loads(response)
+    rand_index = random.randint(0,19)
+    url = loaded_json['data'][rand_index]['images']['fixed_width']['url']
+    return url
 
+# accept the content of msg split into array
 def normalize_query(arr):
     query = '+'.join(arr[2:])
     return query.lower()
@@ -41,8 +54,3 @@ def normalize_query(arr):
 # Print each message the user receives
 # This is a blocking call that will run forever
 client.call_on_each_message(lambda msg: respond(msg))
-
-# Print every event relevant to the user
-# This is a blocking call that will run forever
-# This will never be reached unless you comment out the previous line
-#client.call_on_each_event(lambda msg: sys.stdout.write(str(msg) + "\n"))
